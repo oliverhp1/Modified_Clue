b820c97f1b388a7d7b478b7034f12b06de9a593c
Oliver Wang <oliverhp1@gmail.com>
2020-04-16 01:58:01 -0700

    suggestion functionality first pass

--- GamePlay.cpp
+++ GamePlay.cpp
@@ -7,12 +7,111 @@ using namespace std;
 /** These are all the actions that a player might take on their turn.
  * They are implemented as static methods so we don't have to worry about
  * keeping track of a separate object instance and calling from it.
+ * The overall driver is a method 'execute_turn', which calls the other methods
  * Methods correspond to general Clue gameplay:
  *	navigate
  *	suggest
  *	accuse
  *	pass
  */
+
+
+
+/* 
+	this method is the overall driver of a player's turn.
+	it will call static methods from GamePlay depending on what action
+	the player chooses to take.
+	messages will also be pushed out to all other players.
+
+
+ on a player's turn they have several options:
+ 	0. look at their hand (for simplicity, just show once for now)
+	1. navigate 
+		- from hallway to room (must do this if you start in a hallway)
+		- from room to hallway (if not occupied)
+			- *you must move from the room, unless you were moved there by a suggestion
+		- take secret passageway (if available)
+	2. make suggestion
+		- you can suggest before or after navigating,
+			as long as you are in a room.
+		    but you cannot navigate after suggesting.
+		- when you make a suggestion, the suggested player (and weapon) is moved to your room
+		- then, subsequent player must show you a card you suggested
+			- if they have none, it goes to the next player
+			- if nobody has any, suggestion use case ends.
+			- suggestion use case also ends as soon as first card is shown.
+	3. make accusation
+		- you can accuse at any time.
+		- immediately after an accusation, you either win, or are out of the game permanently
+			- so that of course means you cannot navigate or suggest afterwards.
+*/
+
+void GamePlay::execute_turn(int player_id, Server server, Player* players){
+	// ask what player wants to do
+	string message = "Your turn.  \n"
+		"Do you want to navigate, suggest, accuse, or pass?\r\n\t";
+
+	string invalid_choice = "Invalid choice. Choose again: \n\t";
+
+
+	// should add an option here to check hand again
+	// also perhaps to check all player locations
+	
+
+	int* socket_tracker = server.get_socket_tracker();
+	int socket_id = socket_tracker[player_id];
+	
+	send(
+		socket_id, 
+		message.c_str(), 
+		message.size(), 
+		0
+	);
+
+	// get response
+	bool turn_complete = false;
+
+	while (!turn_complete){
+		turn_complete = true;	// assume it passes; modify otherwise
+
+		string action_buffer = server.receive_communication(socket_id);
+
+		// switch case would be nice, but strings are not supported
+		// perhaps pass in ints later when we use the GUI, then we can do switch case
+
+		if (action_buffer.compare("navigate") == 0){
+			navigate(player_id, socket_tracker, server);
+		}
+		else if (action_buffer.compare("suggest") == 0){
+			suggest(player_id, socket_tracker, server, players);
+		}
+		else if (action_buffer.compare("accuse") == 0){
+			accuse(player_id, socket_tracker, server);
+		}
+		else if (action_buffer.compare("pass") == 0){
+			pass(player_id, socket_tracker, server);
+		}
+		else if (action_buffer.compare("I win") == 0){
+			players[player_id].set_win();
+		}
+		else {
+			turn_complete = false;
+			
+			send(
+				socket_id,
+				invalid_choice.c_str(),
+				invalid_choice.size(),
+				0
+			);
+		}
+	}
+
+
+	return ;
+}
+
+
+
 
 void GamePlay::navigate(int player_id, int* socket_tracker, Server server){
 	int socket_id = socket_tracker[player_id];
@@ -21,7 +120,7 @@ void GamePlay::navigate(int player_id, int* socket_tracker, Server server){
 
 	// TODO: FIGURE OUT VALID CHOICES TO MOVE TO BEFOREHAND. 
 	// please do this in the Player.cpp file, under execute_turn() method if possible.
-	// you'll probably want to make an attribute (e.g. static attribute in GamePlay) tracking where all players locations are.
+	// you'll probably want to make an attribute (e.g. static attribute in GamePlay) tracking where all locations are.
 	// for the logic, refer to the diagram and rules in the file describing our class project.
 	// you can use location_map.  store a player's current location in an attribute, 
 	// e.g. player::location (preferably, make it a private attribute and use setter and getter methods),
@@ -30,8 +129,6 @@ void GamePlay::navigate(int player_id, int* socket_tracker, Server server){
 	// syntax for location map is just: location_map[1] will return "Study", and so on. (see bottom of this file)
 	// 
 	// e.g. "Would you like to move to the " + option1 + " or"... etc
-
-	string request_location = "Where would you like to move?\n\t";
 
 	send(
 		socket_id, 
@@ -76,19 +173,151 @@ void GamePlay::navigate(int player_id, int* socket_tracker, Server server){
 
 
 
-int GamePlay::suggest(int player_id, int* socket_tracker, Server server){
-	cout << "not implemented yet" << endl;
+/** when a player makes a suggestion:
+ *	we retrieve their location, as well as their guesses for character/weapon
+ *	we move the suggested character to the room
+ *  loop over all active players until one of them has one of the cards
+ *  player must then show that card (or decide which one if multiple match)
+ */
+int GamePlay::suggest(int player_id, int* socket_tracker, Server server, Player* players){
+	int socket_id = socket_tracker[player_id];
+	int n_clients = server.get_n_clients();
+	int player_suggestion, weapon_suggestion;
+	string player_buffer, weapon_buffer;
+
+	int location = players[player_id].get_location();
+
+
+	// get player's suggestion for character
+	bool success = false;
+
+	while (!success){
+		send(
+			socket_id, 
+			request_player.c_str(), 
+			request_player.size(), 
+			0
+		);
+
+		player_buffer = server.receive_communication(socket_id);
+		istringstream(player_buffer) >> player_suggestion;
+
+		// validate input (with the GUI this will probably not be necessary,
+		// it will be a conditional based on where the player clicks)
+		if ((player_suggestion > 0) && (player_suggestion < 7)){
+			success = true;
+		}
+		else {
+			send(
+				socket_id,
+				invalid_suggestion.c_str(),
+				invalid_suggestion.size(),
+				0
+			);
+		}
+	}
+	
+	// get player's suggestion for weapon
+	success = false;
+	while (!success){
+		send(
+			socket_id, 
+			request_weapon.c_str(), 
+			request_weapon.size(), 
+			0
+		);
+
+		weapon_buffer = server.receive_communication(socket_id);
+		istringstream(weapon_buffer) >> weapon_suggestion;
+
+
+		// validate input
+		if ((weapon_suggestion > 6) && (weapon_suggestion < 13)){
+			success = true;
+		}
+
+		
+
+		else {
+			string please = weapon_buffer + " is invalid\n\t";
+		
+			send(
+				socket_id,
+				please.c_str(),
+				please.size(),
+				0
+			);
+		}
+	}
+	
+
+
+	/* TODO: 
+		actually move the player there.
+		do this by updating the player attribute for location
+	*/
+
+
+
+
+
+	// communicate to all other clients
+	string notify_all_clients = "Player " + to_string(player_id) 
+		+ " suggests: " + to_string(location) + ", " 
+		+ player_buffer + ", " + weapon_buffer + "\n";
+
+	for (int i = 0; i < n_clients; i++){
+		if (i != player_id){
+			send(
+				socket_tracker[i],
+				notify_all_clients.c_str(),
+				notify_all_clients.size(),
+				0
+			);
+		}
+	}
+
+	// also output on the server
+	cout << notify_all_clients;
+
+
+	// finally, loop over all other players
+	// when one of them shows any card, break
+
+
 	return 0;
 }
+
+
+
 
 int GamePlay::accuse(int player_id, int* socket_tracker, Server server){
 	cout << "not implemented yet" << endl;
 	return 0;
 }
 
-int GamePlay::pass(int player_id, int* socket_tracker, Server server){
-	cout << "not implemented yet" << endl;
-	return 0;
+
+// generally, at the end of your turn, you have the option to accuse.
+// calling this will let you bypass that option
+void GamePlay::pass(int player_id, int* socket_tracker, Server server){
+	// just communicate to all other clients
+	string notify_all_clients = "Player " + to_string(player_id) 
+		+ "\'s turn has ended.\n";
+
+	for (int i = 0; i < server.get_n_clients(); i++){
+		if (i != player_id){
+			send(
+				socket_tracker[i],
+				notify_all_clients.c_str(),
+				notify_all_clients.size(),
+				0
+			);
+		}
+	}
+
+	// also output on the server
+	cout << notify_all_clients;
+	return;
 }
 
 
@@ -99,6 +328,25 @@ string GamePlay::getting_accused(int player_id, int* socket_tracker){
 }
 
 
+void GamePlay::set_player_character(int player_id, Player* players){
+	players[player_id].set_character(card_map[player_id + 1]);
+}
+        
+
+
+// other static attributes
+string GamePlay::request_location = "Where would you like to move?\n\t";
+string GamePlay::request_player = "Who would you like to suggest?\n\t";
+string GamePlay::request_weapon = "With what weapon?\n\t";
+string GamePlay::invalid_suggestion = "Invalid suggestion, try again:\n\t";
+
+
+// it'd be nice tostore players here so we don't have to pass it back and forth
+// when executing gameplay logic
+// but circular includes and forward declarations don't work with this logic
+// void GamePlay::finalize_players(Player *final_players){
+// 	players = final_players;
+// }
 
 // store and populate maps for relevant functions here
 unordered_map<int, string> GamePlay::location_map;
--- Player.cpp
+++ Player.cpp
@@ -11,99 +11,6 @@ Player::Player() {
 
 
 
-/* 
-	this method is the overall driver of a player's turn.
-	it will call static methods from GamePlay depending on what action
-	the player chooses to take.
-	messages will also be pushed out to all other players.
-
-
- on a player's turn they have several options:
- 	0. look at their hand (for simplicity, just show once for now)
-	1. navigate 
-		- from hallway to room (must do this if you start in a hallway)
-		- from room to hallway (if not occupied)
-			- *you must move from the room, unless you were moved there by a suggestion
-		- take secret passageway (if available)
-	2. make suggestion
-		- you can suggest before or after navigating,
-			as long as you are in a room.
-		    but you cannot navigate after suggesting.
-		- when you make a suggestion, the suggested player (and weapon) is moved to your room
-		- then, subsequent player must show you a card you suggested
-			- if they have none, it goes to the next player
-			- if nobody has any, suggestion use case ends.
-			- suggestion use case also ends as soon as first card is shown.
-	3. make accusation
-		- you can accuse at any time.
-		- immediately after an accusation, you either win, or are out of the game permanently
-			- so that of course means you cannot navigate or suggest afterwards.
-*/
-
-void Player::execute_turn(Server server){
-	// ask what player wants to do
-	string message = "Your turn.  \n"
-		"Do you want to navigate, suggest, accuse, or pass?\r\n\t";
-
-	string invalid_choice = "Invalid choice. Choose again: \n\t";
-
-
-	// should add an option here to check hand again
-
-	int* socket_tracker = server.get_socket_tracker();
-	
-	send(
-		socket_id, 
-		message.c_str(), 
-		message.size(), 
-		0
-	);
-
-	// get response
-	bool turn_complete = false;
-
-	while (!turn_complete){
-		turn_complete = true;	// assume it passes; modify otherwise
-
-		string action_buffer = server.receive_communication(socket_id);
-
-		// switch case would be nice, but strings are not supported
-		// perhaps pass in ints later when we use the GUI, then we can do switch case
-
-		if (action_buffer.compare("navigate") == 0){
-			GamePlay::navigate(player_id, socket_tracker, server);
-		}
-		else if (action_buffer.compare("suggest") == 0){
-			GamePlay::suggest(player_id, socket_tracker, server);
-		}
-		else if (action_buffer.compare("accuse") == 0){
-			GamePlay::accuse(player_id, socket_tracker, server);
-		}
-		else if (action_buffer.compare("pass") == 0){
-			GamePlay::pass(player_id, socket_tracker, server);
-		}
-		else if (action_buffer.compare("I win") == 0){
-			win = true;
-		}
-		else {
-			turn_complete = false;
-			
-			send(
-				socket_id,
-				invalid_choice.c_str(),
-				invalid_choice.size(),
-				0
-			);
-		}
-	}
-
-
-	return ;
-}
-
-
-
-
 
 
 
@@ -113,7 +20,6 @@ void Player::execute_turn(Server server){
 // we can also set player's character here
 void Player::set_player_id(int player_id){
 	this->player_id = player_id;
-	this->character = GamePlay::card_map[player_id + 1];
 
 	cout << "Player " << player_id << " initialized" << endl;
 }
@@ -122,6 +28,17 @@ void Player::set_socket_id(int socket_id){
 	this->socket_id = socket_id;
 }
 
+void Player::set_character(string character){
+	this->character = character;
+}
+
+void Player::set_location(int location){
+	this->location = location;
+}
+
+int Player::get_location(){
+	return this->location;
+}
 string Player::get_character(){
 	return this->character;
 }
@@ -134,4 +51,6 @@ bool Player::did_win(){
 	return win;
 }
 
-
+void Player::set_win(){
+	this->win = true;
+}
